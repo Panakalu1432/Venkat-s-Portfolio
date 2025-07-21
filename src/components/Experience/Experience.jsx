e49d6eccb6137dbc30bad5f259b7b6edf8e95ac5
Panakalu <Panakalu1432@gmail.com>
2025-07-21 14:14:32 +0530

    added new file

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -10,8 +10,8 @@ import history from "../../data/history.json";
 export const Experience = () => {
    
   return (
-    <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Experience</h2>
+    <section className={styles.container} id="skills">
+      <h2 className={styles.title}>Skills</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
           {skills.map((skill, id) => {
@@ -24,28 +24,7 @@ export const Experience = () => {
               </div>
             );
           })}
-        </div>
-        <ul className={styles.history}>
-          {history.map((historyItem, id) => {
-            return (
-              <li key={id} className={styles.historyItem}>
-                <img
-                  src={`/assets/${historyItem.imageSrc}`} 
-                  alt={`${historyItem.organisation} Logo`}
-                />
-                <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li key={id}>{experience}</li>;
-                    })}
-                  </ul>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        </div> 
       </div>
     </section>
   );
